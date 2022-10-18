346a75c6d5dd296190950b199e2566242ad53514
Vincent ASTA <vasta@stello.eu>
2022-10-18 18:01:47 +0200

    feat: adding notification in comments form

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -13,6 +13,8 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\File\Exception\FileException;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Notifier\Notification\Notification;
+use Symfony\Component\Notifier\NotifierInterface;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Messenger\MessageBusInterface;
 use Twig\Environment;
@@ -43,7 +45,7 @@ class ConferenceController extends AbstractController
     }
 
     #[Route('/conference/{slug}', 'conference')]
-    public function show(Request $request, Conference $conference, ConferenceRepository $conferenceRepository, CommentRepository $commentRepository, string $photoDir): Response
+    public function show(Request $request, Conference $conference, ConferenceRepository $conferenceRepository, CommentRepository $commentRepository, string $photoDir, NotifierInterface $notifier): Response
     {
 
         $comment = new Comment();
@@ -74,10 +76,14 @@ class ConferenceController extends AbstractController
             ];
 
             $this->bus->dispatch(new CommentMessage($comment->getId(), $context));
+            $notifier->send(new Notification('Thank you for the feedback; your comment will be posted after moderation.', ['browser']));
 
             return $this->redirectToRoute('conference', ['slug' => $conference->getSlug()]);
         }
 
+        if ($form->isSubmitted()){
+            $notifier->send(new Notification('Can you check your submission? There are some problems with it.', ['browser']));
+        }
 
         $offset = max(0, $request->query->getInt('offset', 0));
         $paginator = $commentRepository->getCommentPaginator($conference, $offset);
